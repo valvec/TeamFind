c6f31719dcab5005cc0b1621a52263856889dade
Val Vec <val.vec1@gmail.com>
2022-12-30 16:58:37 +0100

    30-12

--- app/src/main/java/com/example/teamfind/MainActivity.java
+++ app/src/main/java/com/example/teamfind/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
@@ -25,19 +26,46 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
     MainActivity mainActivity;
+
+    SharedPreferences sharedPref;
+    SharedPreferences.Editor editor;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.mainActivity=this;
 
+        sharedPref = getPreferences(Context.MODE_PRIVATE);
+        String username = sharedPref.getString("Username", "---");
+        String pass = sharedPref.getString("Password", "---");
+        editor = sharedPref.edit();
+        if (username.equals("---") || pass.equals("---")){
+        }
+        else {
+            TextView tv = findViewById(R.id.username);
+            tv.setText(username);
+            tv = findViewById(R.id.pass);
+            tv.setText(pass);
+            Login();
+        }
+
+
     }
 
-    public void Login(View view) {
+
+
+
+    public void Login() {
         TextView username_view = findViewById(R.id.username);
         TextView pass_view = findViewById(R.id.pass);
         String username = username_view.getText().toString();
         String pass=pass_view.getText().toString();
+
+        editor.putString("Username",username);
+        editor.putString("Password",pass);
+        editor.apply();
 
         Login login= new Login(username,pass, mainActivity);
         new Thread(){
@@ -50,6 +78,9 @@ public class MainActivity extends AppCompatActivity {
 
             }.start();
         }
+
+
+
         public void onUIThread(String sucess){
             Toast.makeText(getApplicationContext(),sucess,Toast.LENGTH_LONG).show();
             //Toast.makeText(getApplicationContext(),Global.username2,Toast.LENGTH_LONG).show();
@@ -66,6 +97,10 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, register.class);
         startActivity(intent);
 
+    }
+
+    public void Click(View view) {
+        Login();
     }
 }
 
